a8f595a3227105a18f33e15162e4173c7c454a17
Abhilasha Tandon <abhilashatandon167@gmail.com>
2026-04-15 20:47:50 -0400

    Added xlock and apsi to run script

--- run.sh
+++ run.sh
@@ -1,21 +1,18 @@
-rm ./results/povray_bimod.txt
-rm ./results/povray_opcode.txt
-rm ./results/xdoom_bimod.txt
-rm ./results/xdoom_opcode.txt
+rm ./results/*
 
 cd simplesim-3.0/
-# make clean
+make clean
 make config-alpha
-# make
+make
 /usr/bin/gcc `./sysprobe -flags` -DDEBUG -O0 -g -Wall   -c sim-bpred.c
 /usr/bin/gcc -o sim-bpred `./sysprobe -flags` -DDEBUG -O0 -g -Wall   sim-bpred.o bpred.o main.o syscall.o memory.o regs.o loader.o endian.o dlite.o symbol.o eval.o options.o stats.o eio.o range.o misc.o machine.o libexo/libexo.a `./sysprobe -libs` -lm
 
 cd ../xbenchmarks/povray/exe
-for ((i = 1 ; i < 16384 ; i*=2));
+for ((i = 8 ; i < 65536 ; i*=2));
 do
     ../../../simplesim-3.0/sim-bpred -bpred opcode -redir:sim ../../../results/povray_opcode.txt -bpred:opcode $i ./x-povray povray.ini -W320 -H200 -F +D -Q4 +I showoff/mist.pov 
 done
-for ((i = 1 ; i < 16384 ; i*=2));
+for ((i = 8 ; i < 65536 ; i*=2));
 do
     ../../../simplesim-3.0/sim-bpred -bpred bimod -redir:sim ../../../results/povray_bimod.txt -bpred:bimod $i ./x-povray povray.ini -W320 -H200 -F +D -Q4 +I showoff/mist.pov 
 done
@@ -23,13 +20,46 @@ cd ../..
 
 
 cd xdoom/exe
-for ((i = 1 ; i < 16384 ; i*=2));
+for ((i = 8 ; i < 65536 ; i*=2));
 do
     ../../../simplesim-3.0/sim-bpred -bpred opcode -redir:sim ../../../results/xdoom_opcode.txt -bpred:opcode $i ./alphadoom -playdemo rockin
 done
-for ((i = 1 ; i < 16384 ; i*=2));
+for ((i = 8 ; i < 65536 ; i*=2));
 do
     ../../../simplesim-3.0/sim-bpred -bpred bimod -redir:sim ../../../results/xdoom_bimod.txt -bpred:bimod $i ./alphadoom -playdemo rockin
 done
 cd ../..
 
+
+cd xlock/exe
+for ((i = 8 ; i < 65536 ; i*=2));
+do
+    ../../../simplesim-3.0/sim-bpred -bpred opcode -redir:sim ../../../results/xlock_bouboule_opcode.txt -bpred:opcode $i ./xlock -inwindow -nolock -delay 0 -mode bouboule
+done
+for ((i = 8 ; i < 65536 ; i*=2));
+do
+    ../../../simplesim-3.0/sim-bpred -bpred bimod -redir:sim ../../../results/xlock_bouboule_bimod.txt -bpred:bimod $i ./xlock -inwindow -nolock -delay 0 -mode bouboule
+done
+cd ../..
+
+cd ..
+
+cd simplesim-3.0/
+make clean
+make config-pisa
+make
+/usr/bin/gcc `./sysprobe -flags` -DDEBUG -O0 -g -Wall   -c sim-bpred.c
+/usr/bin/gcc -o sim-bpred `./sysprobe -flags` -DDEBUG -O0 -g -Wall   sim-bpred.o bpred.o main.o syscall.o memory.o regs.o loader.o endian.o dlite.o symbol.o eval.o options.o stats.o eio.o range.o misc.o machine.o libexo/libexo.a `./sysprobe -libs` -lm
+cd ..
+
+cd ./spec95-little/
+for ((i = 8 ; i < 65536 ; i*=2));
+do
+    ../simplesim-3.0/sim-bpred -bpred opcode -redir:sim ../results/apsi_opcode.txt -bpred:opcode $i apsi.ss
+done
+for ((i = 8 ; i < 65536 ; i*=2));
+do
+    ../simplesim-3.0/sim-bpred -bpred bimod -redir:sim ../results/apsi_bimod.txt -bpred:bimod $i apsi.ss
+done
+
+cd ../..
